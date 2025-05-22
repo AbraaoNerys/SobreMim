0df1f3fffe7b729811de243f5cc533fda151f434
AbraaoNerys <abraaonerysfrutuoso123@gmail.com>
2025-05-22 15:04:40 -0300

    Update meuporte.js

--- ProjetoRespositoria/meuporte.js
+++ ProjetoRespositoria/meuporte.js
@@ -1,4 +1,25 @@
 document.getElementById("menu-toggle").addEventListener("click", function() {
     document.querySelector(".nav-links").classList.toggle("active");
   });
+
+  const toggleButton = document.getElementById('dark-mode-toggle');
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  
+  // Opcional: salvar preferência no localStorage
+  if(document.body.classList.contains('dark-mode')) {
+    localStorage.setItem('theme', 'dark');
+  } else {
+    localStorage.setItem('theme', 'light');
+  }
+});
+
+// Opcional: carregar preferência salva
+window.addEventListener('load', () => {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === 'dark') {
+    document.body.classList.add('dark-mode');
+  }
+});
+
   
